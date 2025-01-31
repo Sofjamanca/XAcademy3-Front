aa2c736c9b03e5bd439ccab5f3df61991de51cac
Sofjamanca <149734691+Sofjamanca@users.noreply.github.com>
2025-01-31 23:55:52 +0800

    Agregado ApiService para conexión con backend

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'http://localhost:3001/'
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
+
+  register(name: string, lastname: string, email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, { name, lastname, email, password });
+  }
+
+  resetPassword(email: string, oldPassword: string, newPassword: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/reset-password`, { email, oldPassword, newPassword });
+  }
+}
