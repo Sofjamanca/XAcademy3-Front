fbddef2ba104982a551b88770cfa688fb31ab59d
gianlucafarias <72280809+gianlucafarias@users.noreply.github.com>
2025-02-04 18:42:51 -0300

    merge cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { CommonModule } from '@angular/common';  
-
+import { NgFor } from '@angular/common';
 import { CardComponent } from './shared/components/card/card.component';
 
 import { HeaderComponent } from './shared/components/header/header.component';
@@ -17,7 +17,7 @@ import { LandingPageComponent } from "./views/landing-page/landing-page.componen
   selector: 'app-root',
   standalone: true,
   imports: [
-    HeaderComponent,LandingPageComponent, CardComponent,
+    HeaderComponent,LandingPageComponent, CardComponent, NgFor,
     RouterOutlet, MatButtonModule, MatDividerModule, MatGridListModule
     ,MatIconModule, MatToolbarModule, FooterComponent, CommonModule
   ],
@@ -27,7 +27,24 @@ import { LandingPageComponent } from "./views/landing-page/landing-page.componen
 export class AppComponent {
   title = 'XAcademy3-Front';
 
-  
+  cursos = [
+    {
+      title: 'Curso de Plomería',
+      image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fplumber&psig=AOvVaw0900000000000000000&ust=1717416000000000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKC814y00YgDFQAAAAAdAAAAABAE',
+      content: 'Aprende a instalar tuberías y sistemas de agua en tu hogar',
+      actions: ['Accion 1', 'Accion 2']
+    },
+    {
+      title: 'Curso de Electricidad',
+      content: 'Aprende a instalar cables y sistemas eléctricos en tu hogar',
+      actions: ['Accion 1', 'Accion 2']
+    },
+    {
+      title: 'Curso de Ceramica',
+      content: 'Aprende todo sobre la ceramica',
+      actions: ['Accion 1', 'Accion 2']
+    }
+  ]
   
 
   
