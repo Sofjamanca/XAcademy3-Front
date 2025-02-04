1e045de4756d3ef57315a7f24b9a8ce04a2a96e0
gianlucafarias <72280809+gianlucafarias@users.noreply.github.com>
2025-02-04 18:35:53 -0300

    fix cards

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule, 
+    MatButtonModule
+  ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+  @Input() title: string = '';
+  @Input() content: string = '';
+  @Input() actions: string[] = [];
+  @Input() image: string = '';
 
+  @Output() actionClick = new EventEmitter<string>();
+
+  onActionClick(action: string) {
+    this.actionClick.emit(action);
+  }
 }
