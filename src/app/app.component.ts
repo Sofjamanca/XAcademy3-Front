09b41d3227bb11c77cd4565c9012ea92d9412e99
Lauper <64489452+LauPerovich@users.noreply.github.com>
2025-02-11 11:40:49 -0300

    Se establece la ruta para la vista cursos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,18 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from "./views/landing-page/landing-page.component";
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    LandingPageComponent, RouterOutlet, CommonModule
+    // LandingPageComponent,
+    RouterOutlet,
+    CommonModule,
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
