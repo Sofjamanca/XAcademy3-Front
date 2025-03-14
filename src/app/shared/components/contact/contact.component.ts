9d3f9e037c99000be17b90d464eb76117b918c56
Sofjamanca <149734691+Sofjamanca@users.noreply.github.com>
2025-03-14 11:35:31 -0300

    cambios en la página de contact

--- src/app/shared/components/contact/contact.component.ts
+++ src/app/shared/components/contact/contact.component.ts
@@ -5,6 +5,7 @@ import { NgIf, NgForOf, NgClass } from '@angular/common';
 import { MaterialModule } from '../../../material/material.module';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DeviceHelper } from '../../../core/models/helpers/device-helper';
+import emailjs from 'emailjs-com'
 
 @Component({
   selector: 'app-contact',
@@ -78,12 +79,33 @@ export class ContactComponent implements OnInit{
   }
 
   onSubmit(form: NgForm) {
+    console.log(form);
     if (form.valid) {
-      this.snackBar.open('✅ Gracias por tu mensaje. Nos pondremos en contacto pronto.', 'Cerrar', {
-        duration: 3000, 
-        panelClass: ['success-snackbar']
-      });
-      form.reset();
+      const formData = {
+        name: form.value.name,
+        email: form.value.email,
+        phone: form.value.phone,
+        message: form.value.message,
+        time: new Date().toLocaleString(), 
+      };
+
+
+      emailjs.send('service_vf7d7lk', 'template_y2qfuzc', formData, 'gERchL2IHqJiGIhkj')
+        .then(response => {
+          console.log('Mensaje enviado con éxito!', response);
+          this.snackBar.open('✅ Gracias por tu mensaje. Nos pondremos en contacto pronto.', 'Cerrar', {
+            duration: 3000,
+            panelClass: ['success-snackbar']
+          });
+          form.reset();
+        })
+        .catch(error => {
+          console.error('Error al enviar el mensaje', error);
+          this.snackBar.open('⚠️ Ocurrió un error al enviar el mensaje. Intenta nuevamente.', 'Cerrar', {
+            duration: 3000,
+            panelClass: ['error-snackbar']
+          });
+        });
     } else {
       this.snackBar.open('⚠️ Por favor, completa todos los campos.', 'Cerrar', {
         duration: 3000,
