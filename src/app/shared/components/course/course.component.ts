9c778ffbee1b41e857fc4d798887f0a2f288d858
gianlucafarias <palmiergianluca@gmail.com>
2025-03-25 15:28:56 -0300

    cambios diseño admin

--- src/app/shared/components/course/course.component.ts
+++ src/app/shared/components/course/course.component.ts
@@ -78,7 +78,7 @@ export class CourseComponent implements OnInit {
 
           if (this.course.teacher_id) {
             this.teacherService
-              .getTeacherById(this.course.teacher_id)
+              .getTeacherByIdCourse(this.course.teacher_id)
               .subscribe({
                 next: (teacher) => {
                   this.course.teacherName = teacher.user?.name || 'Desconocido';
